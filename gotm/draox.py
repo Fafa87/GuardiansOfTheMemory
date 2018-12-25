f25716468f64bbc1c3dc0455ad793313f23d4dfb
qwerty <fafafft@gmail.com>
2018-12-25 22:02:47 +0100

    release?

--- gotm/draox.py
+++ gotm/draox.py
@@ -15,10 +15,10 @@ def roar():
 
 
 def draox_sentry(angry):
-    mem, sw = xandor.get_current_memory()
+    mem, sw = gotm.xandor.get_current_memory()
     # do not kill if angry (so only one is killed)
     if mem["taken"] > THRESHOLD and not angry:
-        big = xandor.find_proc_with_most_memory()
+        big = gotm.xandor.find_proc_with_most_memory()
         print("There is too much memory taken... ", mem["taken"])
         print(big.name(), "is misbehaving... die! die! die!")
         big.kill()
@@ -32,8 +32,8 @@ def draox_sentry(angry):
 
 
 if __name__ == "__main__":
-    if not xandor.is_root() and not xandor.has_debugger():
-        xandor.elevate_me()
+    if not gotm.xandor.is_root() and not gotm.xandor.has_debugger():
+        gotm.xandor.elevate_me()
         exit(0)
 
     print("Draox starts to defend...")
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     setup_requires=[],
     url="https://github.com/Fafa87/GuardiansOfTheMemory",
-    version="1.0.2"
+    version="1.0.3"
 )
 
 
